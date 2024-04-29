41378305f68ad62f821c4f3c9d5e0ed75afeb3e4
AkshatIyer <akshatviyer@gmail.com>
2024-04-29 00:30:35 -0400

    changed url

--- constants/apiConstants.js
+++ constants/apiConstants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://pebble-inc-2daa7b85d6f9.herokuapp.com";
 export const ACCESS_TOKEN_NAME = 'login_access_token';
