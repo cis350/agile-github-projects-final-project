ca845f0d4b45e80eebe428b350d3e1d37812e217
AkshatIyer <akshatviyer@gmail.com>
2024-04-29 00:37:05 -0400

    deploy backend for heroku

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ require("./server/app/routes/profile.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
