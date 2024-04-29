06775af32e18e634ced9dc77a328d72e045b559d
awu626 <aywu2003@gmail.com>
2024-04-29 17:35:44 -0400

    e2e done for now

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,10 +1,9 @@
 describe('component_check', () => {
   it('login_check', () => {
-    cy.visit('http://localhost:3000/login')
+    cy.visit('https://cisfinalproject-6odfvk3ki-pebble-inc.vercel.app/login')
     cy.contains('Log In').should('be.visible');
     cy.contains('Email').should('be.visible');
     cy.contains('Password').should('be.visible');
-    cy.contains('Forgot password?').should('be.visible');
     cy.contains('About').should('be.visible');
     cy.contains('Book a Ride').should('be.visible');
     cy.contains('Don\'t have an account? Register').should('be.visible');
@@ -12,7 +11,7 @@ describe('component_check', () => {
   })
 
   it('register_check', () => {
-    cy.visit('http://localhost:3000/register')
+    cy.visit('https://cisfinalproject-6odfvk3ki-pebble-inc.vercel.app/register')
     cy.contains('Register').should('be.visible');
     cy.contains('Email').should('be.visible');
     cy.contains('Password').should('be.visible');
@@ -26,9 +25,16 @@ describe('component_check', () => {
 
 describe('flow_check', () => {
   it('login_flow_check', () => {
-    cy.visit('http://localhost:3000/register')
-    cy.get('input[id="email"]').type('test@gmail.com');
-    cy.get('input[id="password"]').type('testpassword');
-    
+    cy.visit('https://cisfinalproject-6odfvk3ki-pebble-inc.vercel.app/register')
+    cy.get('input[id="email"]').type('rah1@gmail.com');
+    cy.get('input[id="password"]').type('testPassword!');
+    cy.get('input[id="confirmPassword"]').type('testPassword!');
+    cy.get('.text-sm.px-5').click()
+    cy.wait(1000);
+    cy.get('input[id="email"]').type('rah1@gmail.com');
+    cy.get('input[id="password"]').type('testPassword!');
+    cy.get('.text-sm.px-5').click()
+    cy.wait(1000);
+    cy.contains('Share a Ride').should('be.visible');
   })
 })
